db5086706cf6933e4789b60fca66833ba08996a7
Rodrigo Soares <silvars1@gmail.com>
2017-10-20 10:33:28 -0200

    Added show filtering by national broadcast.

--- lib/grid.rb
+++ lib/grid.rb
@@ -13,8 +13,18 @@ class Grid
 
   # Queries for a show based on the region and the airing time.
   def query(region, airing_at)
+    shows = find_shows_by(region.upcase, airing_at)
+    shows = find_shows_by(NATIONAL_ABBREV, airing_at) if shows.empty?
+    shows
+  end
+
+  private
+
+  NATIONAL_ABBREV = 'BR'
+
+  def find_shows_by(region, airing_at)
     @shows.select do |show|
-      show.region.eql?(region.upcase) &&
+      show.region.eql?(region) &&
         Time.parse(airing_at).between?(show.starting_at, show.ending_at)
     end
   end
--- spec/statements_spec.rb
+++ spec/statements_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe Statement do
   let(:raw_schedule_stmt) { 'S "DF" "Bom dia DF" 06:01 07:29' }
-  let(:raw_query_stmt)    { 'Q "DF" 07:00' }
 
   describe '.new' do
     it 'initializes a statement' do
@@ -16,30 +15,43 @@ describe Statement do
   end
 
   describe '#execute!' do
-    let(:valid_answer_stmt) { "A \"DF\" 07:00 \"DF\" \"Bom dia DF\"\n" }
-    let(:noise_answer_stmt) { "A \"DF\" 07:00 noise\n" }
+    let(:raw_national_stmt)     { 'S "BR" "Globo Rural" 05:00 06:00' }
+    let(:raw_query_stmt_sp)     { 'Q "SP" 05:28' }
+    let(:raw_query_stmt_df)     { 'Q "DF" 07:00' }
+    let(:valid_answer_output)   { "A \"DF\" 07:00 \"DF\" \"Bom dia DF\"\n" }
+    let(:valid_national_output) { "A \"SP\" 05:28 \"BR\" \"Globo Rural\"\n" }
+    let(:noise_answer_output)   { "A \"DF\" 07:00 noise\n" }
 
     before(:each) do
       Grid.instance.instance_variable_set(:@shows, [])
       @schedule_stmt = Statement.new(raw_schedule_stmt)
-      @query_stmt = Statement.new(raw_query_stmt)
+      @query_stmt = Statement.new(raw_query_stmt_df)
     end
 
     context 'when entering a scheduling statement' do
       it 'schedules a show' do
         @schedule_stmt.execute!
-        expect(Grid.instance.instance_variable_get(:@shows)).not_to be_empty
+        show = Grid.instance.instance_variable_get(:@shows).sample
+        expect(show).not_to be_nil
+        expect(show.region).to eq('DF')
+        expect(show.name).to eq('Bom dia DF')
       end
     end
 
     context 'when entering a query statement' do
       it 'returns the scheduled show if it exists' do
         @schedule_stmt.execute!
-        expect { @query_stmt.execute! }.to output(valid_answer_stmt).to_stdout
+        expect { @query_stmt.execute! }.to output(valid_answer_output).to_stdout
       end
 
       it 'returns a noise answer if it does not exist' do
-        expect { @query_stmt.execute! }.to output(noise_answer_stmt).to_stdout
+        expect { @query_stmt.execute! }.to output(noise_answer_output).to_stdout
+      end
+
+      it 'returns a national show if a regional does not exist' do
+        Statement.new(raw_national_stmt).execute!
+        sp_stmt = Statement.new(raw_query_stmt_sp)
+        expect { sp_stmt.execute! }.to output(valid_national_output).to_stdout
       end
     end
   end
